e4505e30a159f696c73a878573a7f4a9cf601ba2
Ivans Mihailovs <ivans.mihailovs@conclusionintelligence.de>
2026-02-24 18:48:33 +0100

    Initial Commit

---
+++ GetWellGalaxy/GetWellGalaxyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetWellGalaxyApp.swift
+//  GetWellGalaxy
+//
+//  Created by Ivans Mihailovs on 24/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GetWellGalaxyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

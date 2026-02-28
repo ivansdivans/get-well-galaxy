b48d18282d59ca10da46e921ec96339e200809e5
Ivans Mihailovs <mihailovs.ivans@gmail.com>
2026-02-28 18:53:55 +0100

    feat: implement character card

--- GetWellGalaxy/Features/Characters/Views/CharacterDetailsView.swift
+++ GetWellGalaxy/Features/Characters/Views/CharacterDetailsView.swift
@@ -14,10 +14,35 @@ struct CharacterDetailsView: View {
     var body: some View {
         Group {
             if let character = viewModel.character {
-                VStack {
-                    Text("Character: \(character.name)")
-                    Text("Is: \(character.status)")
+                VStack(alignment: .leading) {
+                    AsyncImage(url: URL(string: character.image)) { phase in
+                        if let image = phase.image {
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(RoundedRectangle(cornerRadius: 20))
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        } else if phase.error != nil {
+                            Image(systemName: "person.crop.square.fill")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundStyle(.secondary)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        } else {
+                            ProgressView()
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        }
+                    }
+                    .frame(height: 300)
+                    
+                    Text("Full Name: \(character.name)")
+                    Text("From: \(character.origin.name)")
+                    Text("Species: \(character.species)")
+                    Text("Currently: \(character.status)")
+                    Text("Appears in: \(character.episode.count) episodes")
+                    Spacer()
                 }
+                .padding(.horizontal, 30)
             } else if viewModel.isLoading {
                 ProgressView()
             } else {
@@ -34,21 +59,21 @@ struct CharacterDetailsView: View {
         }
         .alert(
             .errorAlertTitle,
-           isPresented: Binding(
-               get: { viewModel.errorMessage != nil },
-               set: { isPresented in
-                   if isPresented == false { viewModel.clearError() }
-               }
-           ), actions: {
-               Button(.errorAlertRetryButton) {
-                   Task { await viewModel.retry(with: characterID) }
-               }
-               Button(.errorAlertCancelButton, role: .cancel) {
-                   viewModel.clearError()
-               }
-           }, message: {
-               Text(viewModel.errorMessage ?? "")
-           }
+            isPresented: Binding(
+                get: { viewModel.errorMessage != nil },
+                set: { isPresented in
+                    if isPresented == false { viewModel.clearError() }
+                }
+            ), actions: {
+                Button(.errorAlertRetryButton) {
+                    Task { await viewModel.retry(with: characterID) }
+                }
+                Button(.errorAlertCancelButton, role: .cancel) {
+                    viewModel.clearError()
+                }
+            }, message: {
+                Text(viewModel.errorMessage ?? "")
+            }
         )
     }
 }

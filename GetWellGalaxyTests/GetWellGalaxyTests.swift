0ffb2346315886280972638fd24c5f230e06742b
Ivans Mihailovs <mihailovs.ivans@gmail.com>
2026-03-01 21:35:10 +0100

    feat: add unit testing target

---
+++ GetWellGalaxyTests/GetWellGalaxyTests.swift
@@ -0,0 +1,16 @@
+//
+//  GetWellGalaxyTests.swift
+//  GetWellGalaxyTests
+//
+//  Created by Ivans Mihailovs on 01/03/2026.
+//
+
+import Testing
+
+struct GetWellGalaxyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
